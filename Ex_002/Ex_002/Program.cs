423c2fb71962aaa51f695bc405e9f79dd5f73ddd
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-02-17 16:13:59 -0300

    Atividade 2

---
+++ Ex_002/Ex_002/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace Ex_002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de A:");
+            float A = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de B:");
+            float B = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de C:");
+            float C = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float pi = 3.14159F;
+
+            float area_triangulo = (A * C) / 2;
+            float area_circulo = pi * (C * C);
+            float area_trapezio = ((A + B) * C) / 2;
+            float area_quadrado = B * B;
+            float area_retangulo = A * B;
+
+            Console.WriteLine("TRIÂNGULO: " + area_triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + area_circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPÉZIO: " + area_trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + area_quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETÂNGULO: " + area_retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
--- ex_001/ex_001/Program.cs
+++ ex_001/ex_001/Program.cs
@@ -8,26 +8,29 @@ namespace Ex_001
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha)");
 
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Você digitou {n1}");
+            string[] vet = Console.ReadLine().Split(' ');
 
-            char ch = char.Parse(Console.ReadLine());
-            Console.WriteLine(ch);
+            string ultimo_nome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
-
-            string[] v = Console.ReadLine().Split(' ');
-            string nome = v[0];
-            char sexo = char.Parse(v[1]);
-            int idade = int.Parse(v[2]);
-            double altura = double.Parse(v[3], CultureInfo.InvariantCulture);
             Console.WriteLine(nome);
-            Console.WriteLine(sexo);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco);
+
+            Console.WriteLine(ultimo_nome);
             Console.WriteLine(idade);
             Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
 
+           
 
         }
     }
