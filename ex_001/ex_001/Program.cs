9d6c24b17b80245063a250a410a8d3ab0f613fa3
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-02-17 14:59:32 -0300

    entrada de dados

---
+++ ex_001/ex_001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ex_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            string[] vet = x.Split(" ");
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(x);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+
+
+        }
+    }
+}
