c5fb15bdf2a83f6374945198af712e475e5c21b0
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-02-17 14:03:06 -0300

    primeiro projeto

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de Escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+            
+            Console.WriteLine("Produtos: \n\n{0}, cujo preço é $ {1}\n{2}, cujo preço é $ {3}", produto1, preco1, produto2, preco2);
+            Console.WriteLine($"\nRegistro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine($"\nMedida com oito casa decimais: {medida}");
+            Console.WriteLine($"Arredondando (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
