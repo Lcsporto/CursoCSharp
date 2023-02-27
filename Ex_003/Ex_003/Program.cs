6476d9df6694c082ad7fddd01bca67d29d823b99
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-02-27 11:24:52 -0300

    exercicio if e else

---
+++ Ex_003/Ex_003/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+namespace Ex_003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine()); 
+
+            if (a % b ==0 || b % a == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são multiplos");
+            }
+        }
+    }
+}
