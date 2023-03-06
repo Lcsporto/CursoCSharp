a45599c73b017a9cae561d7510010594f338252c
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-03-06 11:20:43 -0300

    exercicios while

---
+++ Ex_008/Ex_008/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex_008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string quadrante = " ";
+            
+            while (quadrante != "")
+            {
+                Console.Write("Digite o valor de X: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Digite o valor de y: ");
+                int y = int.Parse(Console.ReadLine());
+
+                quadrante = Quadrante(x, y);
+                Console.WriteLine(quadrante);
+            }
+        }
+
+
+        static string Quadrante(int a , int b)
+        {
+            string result;
+
+            if (a > 0 && b > 0)
+            {
+                result = "primeiro";
+            }
+            else if (a < 0 && b > 0)
+            {
+                result = "segundo";
+            }
+            else if (a < 0 && b < 0)
+            {
+                result = "terceiro";
+            }
+            else if (a > 0 && b < 0)
+            {
+                result = "quarto";
+            }
+            else
+            {
+                result = "";
+            }
+            return result;
+
+        }
+
+    }
+}
---
+++ Ex_009/Ex_009/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex_009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int combustivel = 0;
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            while (combustivel != 4) {
+                combustivel = int.Parse(Console.ReadLine());
+
+                if (combustivel == 1)
+                {
+                    alcool += 1;
+                }
+                else if (combustivel == 2) {
+                    gasolina += 1;
+                }
+                else if(combustivel == 3) { 
+                    diesel += 1;
+                }
+            }
+
+            Console.WriteLine("MUITO OBRIGADO \nAlcool:" + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+            
+        }
+    }
+}
