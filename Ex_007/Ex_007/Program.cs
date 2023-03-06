7c9fb81cac02224f854aed37da555faab2bb54e4
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-03-06 10:08:22 -0300

    estudos de while

---
+++ Ex_007/Ex_007/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Ex_007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Inválida");
+                Console.Write("Digite a senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
