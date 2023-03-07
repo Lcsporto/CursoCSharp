70b2f24c7c8309eaaa0bbe2518ee0a7754bcbcb9
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-03-07 11:38:16 -0300

    exercicio de classes e atributos

--- Ex_012/Ex_012/Program.cs
+++ Ex_012/Ex_012/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Ex_012
 {
@@ -6,7 +7,16 @@ namespace Ex_012
     {
         static void Main(string[] args)
         {
-            
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e a altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA: "+ ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO: " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL: " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
