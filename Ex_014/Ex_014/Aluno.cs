fd55eb724f8c21950ea07eac04017ebb0cbdb2fe
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-03-07 12:58:22 -0300

    Ex_014

---
+++ Ex_014/Ex_014/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Ex_014
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double nota1, nota2, nota3;
+
+        public double SomaNotas()
+        {
+            return nota1 + nota2 + nota3;
+        }
+
+        public double NotaQueFalta()
+        {
+            return 60 - SomaNotas() ;
+        }
+    }
+}
---
+++ Ex_014/Ex_014/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex_014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aln = new Aluno();
+
+            Console.Write("nome do aluno:");
+            aln.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aln.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aln.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aln.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + aln.SomaNotas().ToString("F2", CultureInfo.InvariantCulture));
+            if (aln.SomaNotas() > 60)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + aln.NotaQueFalta().ToString("F2", CultureInfo.InvariantCulture) + "PONTOS");
+            }
+
+            
+        }
+    }
+}
