0b8796a9adda476e3041ae250ab2f5caf34c2315
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2023-04-15 12:36:14 -0300

    Exercicio 18 - Vetores

---
+++ Ex_018/Ex_018/Estudante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Ex_018
+{
+    internal class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
