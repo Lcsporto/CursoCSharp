5d141c0f6a183c325bcd32c79968572154b6633a
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-04 07:49:56 -0300

    Exercicios Estrutura While

--- Exercicios/003 Estrutura While/Exercicio_003/Exercicio_003/Program.cs
+++ Exercicios/003 Estrutura While/Exercicio_003/Exercicio_003/Program.cs
@@ -4,10 +4,49 @@
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("-----------------------");
             Console.WriteLine("1. Álcool");
             Console.WriteLine("2. Gasolina");
             Console.WriteLine("3. Diesel");
             Console.WriteLine("4. Sair");
+            Console.WriteLine("-----------------------");
+
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            Console.Write("Digite a opção desejada: ");
+            int cont = int.Parse(Console.ReadLine());
+
+            while (cont != 4)
+            {
+                if (cont == 1)
+                {
+                    alcool += 1;
+                    Console.Write("Digite a opção desejada: ");
+                    cont = int.Parse (Console.ReadLine());
+                }
+                else if (cont == 2) {
+                    gasolina += 1;
+                    Console.Write("Digite a opção desejada: ");
+                    cont = int.Parse(Console.ReadLine());
+                }
+                else if (cont == 3) {
+                    diesel += 1;
+                    Console.Write("Digite a opção desejada: ");
+                    cont = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.Write("Digite a opção desejada: ");
+                    cont = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
 
 
 
