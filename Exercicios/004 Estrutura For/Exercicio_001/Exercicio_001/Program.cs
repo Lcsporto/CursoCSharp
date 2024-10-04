b8f84b51e3e69469a6a443f8656a4f5944cac6ee
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-04 09:44:49 -0300

    Inicio Exercícios Estrutura For

---
+++ Exercicios/004 Estrutura For/Exercicio_001/Exercicio_001/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro entre 1 e 1000: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int impar = 0;
+
+            if(x >= 1 && x <= 1000)
+            {
+                for(int i = 1; i <= x; i++)
+                {
+                    if (i % 2 != 0) {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Número fora do intervalo!");
+            }
+            
+        }
+    }
+}
---
+++ Exercicios/004 Estrutura For/Exercicio_002/Exercicio_002/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio_002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int soma_in = 0;
+            int soma_out = 0;
+            int x = 0;
+
+            Console.Write("Quantos números serão digitados? ");
+            int n = int.Parse(Console.ReadLine());
+                    
+            
+            for (int i = 1; i <= n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                {
+                    soma_in++;
+                }
+                else
+                {
+                    soma_out++;
+                }
+            }
+
+            Console.WriteLine($"{soma_in} in");
+            Console.WriteLine($"{soma_out} out");
+
+        }
+    }
+}
