3d02192f08b8a171751db06107ecbac7d24b031c
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-07 00:25:51 -0300

    Finalização do Problema dos Triangulos

--- Classes/Classes/Triangulo.cs
+++ Classes/Classes/Triangulo.cs
@@ -1,4 +1,6 @@
-﻿namespace Classes
+﻿using System;
+
+namespace Classes
 {
     internal class Triangulo
     {
@@ -6,5 +8,14 @@
         public double A; //public significa que esses atributos poderão ser acessados por outro arquivo. Ex: O programa Principal 'Program.cs'.
         public double B;
         public double C;
+
+        public double Area() // Criando uma função que faz o cálculo da área do triangulo. 
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+        }
+
+
     }
 }
