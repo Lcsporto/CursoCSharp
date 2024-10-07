23b3408eef3e88e7c3aecd591e7aca34ef916308
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-06 22:06:25 -0300

    Teoria de Classes, Objetos e Atributos

---
+++ Classes/Classes/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes
+{
+    internal class Triangulo
+    {
+
+        public double A; //public significa que esses atributos poderão ser acessados por outro arquivo. Ex: O programa Principal 'Program.cs'.
+        public double B;
+        public double C;
+    }
+}
