3121f4fef8f7556b5c7c1393a260850f15fe57eb
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-09 18:26:52 -0300

    Exercicio Membro Estático e finalizando Seção 4

---
+++ Exercicios/007 Membros Estaticos/Exercicio_Fixacao/Exercicio_Fixacao/ConversorDeMoeda.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Exercicio_Fixacao {
+    internal class ConversorDeMoeda {
+
+        public static double Iof = 6.0;
+        public static double Conversao(double cotacao, double compra) {
+            return cotacao * (compra * (1 + (Iof/100.00)));
+        }
+    }
+}
---
+++ Exercicios/007 Membros Estaticos/Exercicio_Fixacao/Exercicio_Fixacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Exercicio_Fixacao {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Qauntos dolares você vai comprar? ");
+            double compra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.Conversao(cotacao, compra);
+
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
