944e7abc49e740b455a813e933bd82da81b51877
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-10 22:03:11 -0300

    Construtores e Sobrecarga

---
+++ Construtores/Construtores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exemplo2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //Construtor é uma operação especial da classe, que executa no momento da instanciação do objeto.
+            //Com o construtor declarado, o progrmador é obrigado a inserir os argumentos na instanciação do objeto.
+            //Produto p = new Produto("TV", 500.0, 10);
+
+            Console.WriteLine("Entre os dados do Produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            /* Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());*/
+
+            Produto p = new Produto(nome, preco); // Com o construtor declarado, o objeto é instanciado depois.
+
+            Console.WriteLine();
+            Console.WriteLine("Produto " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionados no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+        }
+    }
+}
