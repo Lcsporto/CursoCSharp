ab0af14977b24a9d3d82b77bd5e16b557e9f854f
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-17 19:12:07 -0300

    Exercício Fixação Vetores

---
+++ Exercicios/009 Vetores/ExercicioFixacao/ExercicioFixacao/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExercicioFixacao {
+    internal class Aluno {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Aluno(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Email;
+        }
+
+    }
+}
---
+++ Exercicios/009 Vetores/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ExercicioFixacao {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Aluno[] aluno = new Aluno[10]; //Aqui fazemos a instanciação do Vetor de uma classe
+
+            Console.Write("Quantos Quartos serão alugados? ");
+            int num_rooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < num_rooms; i++) {
+
+                Console.WriteLine($"Aluguel #{i + 1}:");
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+
+                
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                aluno[room] = new Aluno(nome, email);//Aqui fazemos a instanciação do objeto de uma classe
+
+            }
+
+            Console.WriteLine("Quartos Ocupados:");
+            for (int i = 0; i < 10; i++) {
+
+                if (aluno[i] != null) {
+                    
+                    Console.WriteLine($"{i}: " + aluno[i]);
+                }
+    
+            }
+
+
+
+        }
+    }
+}
