f55ce392cff441d8290a3ec58cc8d94fcb74b33f
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-22 22:48:11 -0300

    Exercício Matriz

---
+++ Exercicios/011 Matrizes/ExercicioMatriz/ExercicioMatriz/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.AccessControl;
+
+namespace ExercicioMatriz {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string[] num_mat = Console.ReadLine().Split(' ');
+            int m = int.Parse(num_mat[0]);
+            int n = int.Parse(num_mat[1]);
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++) {
+
+                string[] valores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) { 
+                    
+                    if (x == mat[i, j]) {
+                        int left, rigth, up, down;
+
+                        Console.WriteLine($"Position:{i}, {j}: ");
+
+                        if (j > 0) {
+                            left = mat[i, j - 1];
+                            Console.WriteLine($"Left: {left}");
+                        }
+                        
+                        if (i > 0) {
+                            up = mat[i -1, j];
+                            Console.WriteLine($"Up: {up}");
+                        }
+
+                        if (j < n - 1){
+                            rigth = mat[i, j+1];
+                            Console.WriteLine($"Right: {rigth}");
+                        }
+
+                        if (j < m - 1) {
+                            down = mat[i + 1, j];
+                            Console.WriteLine($"Down: {down}");
+                        }  
+
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Matrizes/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Matrizes {
+    internal class Program {
+        static void Main(string[] args) {
+            /*
+             Em progrmação "Matriz" é o nome dado a arranjos bidimencionais
+
+            Arranjo é uma estrutura de dados:
+                Homogênea (dados do mesmo tipo)
+                Ordenada (elementos acessados por meio de posições)
+                Alocada de uma vez só, em um bloco contíguo de memória
+
+            Vantagens 
+                Acesso imediato aos elementos pela sua posição 
+
+            Desvantagens 
+                Tamanho Fixo
+                Dificuldades para se realizar inserções e deleções
+             */
+
+            double[,] mat = new double[2, 3]; //A virgula indica que estamos criando um arranjo bidimensional. Instanciamos igual a um vetor. 
+            
+            Console.WriteLine(mat.Length); //Mostra quantos elementos a matriz tem no total. 
+            Console.WriteLine(mat.Rank); //Quanto é a primeira dimensão da matriz (quantidade de linhas)
+            Console.WriteLine(mat.GetLength(0)); //mostra o tamanho da primeira dimensão (linhas: 2)
+            Console.WriteLine(mat.GetLength(1)); //mostra o tamanho da segunda dimensão  (colunas: 3)
+            
+        }
+    }
+}
