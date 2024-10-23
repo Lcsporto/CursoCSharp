0b2acf75329c7d1922e0fba3236fc659c4ccd1e7
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-22 23:44:21 -0300

    Exercicios Extras

---
+++ Exercicios/011 Matrizes/Atividades Extras/001/Exercicio_001/Exercicio_001/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio_001 {
+    internal class Program {
+        static void Main(string[] args) {
+            //Vetores e Matrizes
+
+            //Dada uma seqüência de n números (vetor de inteiros), imprimi-la na ordem inversa que foi realizada a leitura. 
+
+            Console.Write("Informe a quantidade de números: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------");
+
+            int[] vet = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("----------------------------");
+            for (int i = n; i > 0; i--) {
+                Console.WriteLine(vet[i-1]);
+            }
+
+
+        }
+    }
+}
---
+++ Exercicios/011 Matrizes/Atividades Extras/002/Exercicio_002/Exercicio_002/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace Exercicio_002 {
+    internal class Program {
+        static void Main(string[] args) {
+            /* Dados dois vetores x e y, ambos com n elementos, determinar o produto escalar desses vetores.Ou seja, realizar
+            a soma de todos dos resultados da multiplicação de x[i] por y[i]. */
+
+            Console.Write("Entre a quantidade de elementos: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------");
+
+            int[] x = new int[n];
+            int[] y= new int[n];
+
+            Console.WriteLine("Entre com os valores de x: ");
+            for (int i= 0; i < n; i++) {
+                x[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Entre com os valores de y: ");
+            for (int i = 0; i < n; i++) {
+                y[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("---------------------");
+
+            int soma = 0;
+            for (int i =0; i < n; i++) {
+                int mult = x[i] * y[i];
+                soma += mult;
+            }
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
