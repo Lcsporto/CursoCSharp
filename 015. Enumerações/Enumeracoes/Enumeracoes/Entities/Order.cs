01ffa281903cfeedc93d53454173acae5f0d8136
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-10-27 23:09:56 -0300

    Enumerações

---
+++ 015. Enumerações/Enumeracoes/Enumeracoes/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using Enumeracoes.Entities.Enums;
+using System;
+
+namespace Enumeracoes.Entities // Por padrão, o C# atribui o nome como: Projeto.Pasta
+{
+    internal class Order
+    {
+
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id 
+                + ", " 
+                + Moment 
+                + ", " 
+                + Status;
+        }
+
+    }
+}
