8564524b92f4a3bfa3c260b34a7c64c59ecafecf
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-11-06 23:53:03 -0300

    Explicação prática (parte 1)

---
+++ 017. Upcasting e Downcasting/Upcasting_Downcasting/Upcasting_Downcasting/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Upcasting_Downcasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                    Casting: Converter objeto de um tipo para outro. 
+
+                Upcasting
+                    Casting da subclasse para a superclasse 
+                    Uso comum: polimorfismo
+
+                Downcasting
+                    Casting da Superclasse para a subclasse
+                    Palavra as
+                    Palavra is 
+                    Uso comum: métodos que recebem parâmetros genéricos
+
+                       
+                
+             */
+        }
+    }
+}
