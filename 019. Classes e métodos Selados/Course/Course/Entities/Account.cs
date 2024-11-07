773d4b6c68e2437cea47b27cb9356e32ef851aa0
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-11-07 20:29:35 -0300

    Classes e métodos selados

---
+++ 019. Classes e métodos Selados/Course/Course/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿namespace Course.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void withdraw(double amount)// Colocamos a palavra 'Virtual' para dizer que esse método pode ser sobreposto pelas SubClasses
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
---
+++ 019. Classes e métodos Selados/Course/Course/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using Course.Entities;
+
+namespace Course.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void withdraw(double amount)// Reimplementando a Função da Classe Account
+        {
+            base.withdraw(amount); //utilizando a lógica que já existia em Account com a palavra Base.
+            Balance -= 2.0;
+        }
+
+    }
+}
