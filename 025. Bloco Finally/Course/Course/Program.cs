c6d4aeb8e0cf4bfc6c3692beaf2a761f6459e537
Lucas Lima <98219885+Lcsporto@users.noreply.github.com>
2024-11-15 18:25:11 -0300

    Bloco Finally

---
+++ 025. Bloco Finally/Course/Course/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Course
+{
+    public static void Main()
+    {
+        /*
+             Bloco Finally
+
+                É um bloco que contém código a ser executado independentemente de ter ocorrido ou não uma exceção
+
+                Exemplo Classico: Fechar um arquivo ou conexão de banco de dados ao final do processamento
+             */
+    
+    }
+}
