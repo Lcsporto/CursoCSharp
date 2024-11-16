7013644159c0b92077a84f6003369cb99ce304b4
Lucas Lima <lucas_lp1@outlook.com.br>
2024-11-16 20:30:04 -0300

    Exceções Personalizadas - Parte 2

---
+++ 026. Exceções Personalizadas/Parte_2/Part_1/Entities/Reservation.cs
@@ -0,0 +1,56 @@
+﻿namespace Course.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public string UpdateDates(DateTime checkIn, DateTime checkOut) // A Lógica para Controle das Exceções são postas aqui na Classe
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkIn < now || checkOut < now)
+            {
+                return "Error in reservation: Reservation dates for updates must be future dates";
+            }
+
+            if (checkOut <= checkIn)
+            {
+                return "Error in reservation: Check-our daste must be after Check-in";
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", ckeck-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
