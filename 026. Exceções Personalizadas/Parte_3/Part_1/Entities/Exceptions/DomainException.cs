d41327891a8706baedd2ea1977be9ea9d185b9c2
Lucas Lima <lucas_lp1@outlook.com.br>
2024-11-18 18:34:18 -0300

    Finalizando Teoria de Execeções

---
+++ 026. Exceções Personalizadas/Parte_3/Part_1/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Course.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException // Ela é uma subclasse da Application, que é personalizável. 
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ 026. Exceções Personalizadas/Parte_3/Part_1/Entities/Reservation.cs
@@ -0,0 +1,62 @@
+﻿using Course.Entities.Exceptions;
+
+namespace Course.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Error in reservation: Check-out date must be after Check-in");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut) // A Lógica para Controle das Exceções são postas aqui na Classe
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException ("Error in reservation: Reservation dates for updates must be future dates"); // Lança uma exceção;
+            }
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException ("Error in reservation: Check-out date must be after Check-in");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", ckeck-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
