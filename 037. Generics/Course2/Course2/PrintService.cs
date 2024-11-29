11f8fe2bb619e6fa3794a066f6465bd6515ab0b2
Lucas Lima <lucas_lp1@outlook.com.br>
2024-11-28 21:26:04 -0300

    Generics parte 2

---
+++ 037. Generics/Course2/Course2/PrintService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Course2
+{
+    internal class PrintService<T> // Aqui fazemos a classe sendo de um tipo genérico T. No programa principal, poderemos instanciar com o tipo que Quisermos. 
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("Print service is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Print service is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+
+    }
+}
---
+++ 037. Generics/Course2/Course2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Course2;
+
+namespace Course2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Generics permitem que classes, interfaces e métodos possam ser parametrizados por tipo. Seus benefícios são:
+                    Reuso
+                    Type Safety
+                    Performance
+
+                Uso comum: Coleções
+             */
+
+            PrintService<int> printservice = new PrintService<int>(); // Aqui colocamos entre <> o tipo que queremos instanciar o nosso objeto. 
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printservice.AddValue(x);
+            }
+
+            printservice.Print();
+            Console.WriteLine("First: " + printservice.First());
+        }
+    }
+}
