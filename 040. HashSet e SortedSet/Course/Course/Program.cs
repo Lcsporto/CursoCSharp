b2dd4875e660450f621377e8613524ee71cb2afe
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-02 23:47:00 -0300

    HashSet e SortedSet

---
+++ 040. HashSet e SortedSet/Course/Course/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                HashSet<T> e Sortedset<T>
+
+                Representa um conjunto de elementos (similar ao da Álgebra)
+                    Não admite repetições
+                    Elementos não possuem posição
+                    Acesso, inserção e remoção de elementos são rápidos
+                    Oferece operações eficientes de conjunto: inserção, união, diferença.
+
+
+             */
+        }
+    }
+}
