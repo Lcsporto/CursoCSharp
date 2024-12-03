55208546a8ae8756481678a7cc05afaa298efcbd
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-02 23:58:57 -0300

    Aula 1 - HashSet

--- 040. HashSet e SortedSet/Course/Course/Program.cs
+++ 040. HashSet e SortedSet/Course/Course/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Course
+﻿using System.Collections.Generic;
+
+namespace Course
 {
     internal class Program
     {
@@ -13,8 +15,43 @@
                     Acesso, inserção e remoção de elementos são rápidos
                     Oferece operações eficientes de conjunto: inserção, união, diferença.
 
+                Diferenças 
 
+                HashSet
+                    Armazenamento em tabela hash
+                    Extremamente rápido: inserção, remoção e busca O(1) - apenas um passo para chegar no dado. 
+                    A ordem dos elementos não é garantida
+
+                SortedSet
+                    Armazenamento em árvore
+                    Rápido: inserção, remoção e busca O(log(n)) - Base logarítmica.
+                    Os elementos são armazenados ordenadamente conforme implementação IComparer<T>
+
+                Métodos Importantes: 
+
+                    Add
+                    Clear
+                    Contains
+                    UnionWith(other) - operação união: adiciona no conjunto os elementos do outro conjunto, sem repetição;
+                    IntersectWith(other) - operação de Interseção: remove do conjunto os elementos não contidos em other;
+                    ExceptWith(other) - Operação de diferença: remove do conjunto os elementos contidos em other;
+                    Remove(T)
+                    RemoveWhere(predicate)
              */
+
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook")); //retorna True ou False
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
         }
     }
 }
--- 040. HashSet e SortedSet/Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
+++ 040. HashSet e SortedSet/Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2900feaf37b6bf5baf80d698c10189ff0eb32bf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2dd4875e660450f621377e8613524ee71cb2afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
