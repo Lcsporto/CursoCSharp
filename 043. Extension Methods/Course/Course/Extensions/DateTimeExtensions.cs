4166c3dd2ac4d3f5ebef88302031becd94bcc06f
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-06 19:13:06 -0300

    Extension Methods

---
+++ 043. Extension Methods/Course/Course/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace System //Pelo fato do Datetime ser do namespace System, podemos colocar aqui tbm. 
+{
+    static internal class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj) // O método precisa ser static e é necessário o this. 
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+
+        }
+
+
+    }
+}
