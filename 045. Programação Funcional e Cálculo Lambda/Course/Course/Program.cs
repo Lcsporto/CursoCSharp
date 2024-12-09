d2a843b1bf26c011b74dfe51f152c2781ff1b662
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-09 20:06:47 -0300

    Programação Funcional e Cálculo Lambda

---
+++ 045. Programação Funcional e Cálculo Lambda/Course/Course/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Paradigmas de Progrmação:
+
+                    Imperativo (C, Pascal, Fortran, Cobol);
+                    Orientado a Objetos (C++, Object Pascal, Java (<8), c# (<3))
+                    Funcional (Haskell, Closure, Erlang)
+                    Lógico (Prolog)
+                    Multiprograma (JavaScript, Java (8+), C#(3+), Ruby, Python, GO)
+             */
+        }
+    }
+}
