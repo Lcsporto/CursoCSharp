97f7db2bff1e25d15ccd6d2c5640643015145ef9
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-10 20:19:36 -0300

    Action e Func

---
+++ 046. Introdução a Delegates/003. Action (Exemplo com ForEach)/Course/Course/Program.cs
@@ -0,0 +1,41 @@
+﻿using Course.Entities;
+using System.Collections.Generic;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Representa um método void que recebe zero ou mais argumentos
+
+                 public delegate void Action();
+                 public delegate void Action<in T>(T obj);
+                 public delegate void Action<in T1, in T2>(T1 arg1, T2 arg2);
+                 public delegate void Action<in T1, in T2, in T3>(T1 arg1, T2 arg2, T3 arg3);
+                 (16 sobrecargas)
+             */
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Action<Product> act = UpdatePrice;
+
+            list.ForEach(act);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
---
+++ 046. Introdução a Delegates/004. Func (Exemplo com Select)/Course/Course/Program.cs
@@ -0,0 +1,41 @@
+﻿using Course.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Representa um método que recebe zero ou mais argumentos, e retorna um valor
+
+            public delegate TResult Func<out TResult>();
+            public delegate TResult Func<in T, out TResult>(T obj);
+            public delegate TResult Func<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
+            public delegate TResult Func<in T1, in T2, in T3, out TResult>(T1 arg1, T2 
+            arg2, T3 arg3)
+
+             */
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+
+    }
+}
