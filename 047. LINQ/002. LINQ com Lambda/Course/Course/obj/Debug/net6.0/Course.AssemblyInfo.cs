5a226b5ade08080ce41bb21be7edde16a86e54bf
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-11 22:49:44 -0300

    Finalizando aula De Demonstração de LINQ com Lambda

--- 047. LINQ/002. LINQ com Lambda/Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
+++ 047. LINQ/002. LINQ com Lambda/Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30031d98e7cf7e757ea1f476e7c917522e5d93b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ae93161ef6ec425402d4ca325dcaf45767634e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
