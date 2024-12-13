5e322f99182621a5b834e011ce6f38707f203217
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-13 16:10:40 -0300

    Exercicio resolvido

---
+++ 047. LINQ/002. LINQ com Sintaxe Similar à SQL/Course/Course/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Course.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+    }
+}
