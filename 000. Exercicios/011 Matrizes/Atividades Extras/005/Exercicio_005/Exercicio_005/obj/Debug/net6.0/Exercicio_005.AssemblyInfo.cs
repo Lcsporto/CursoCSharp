e5c24e68e5a85cdd876166694e26d03d1bdcaa3f
Lucas Lima <lucas_lp1@outlook.com.br>
2025-01-07 21:40:04 -0300

    Apenas Aulas

--- 000. Exercicios/011 Matrizes/Atividades Extras/005/Exercicio_005/Exercicio_005/obj/Debug/net6.0/Exercicio_005.AssemblyInfo.cs
+++ 000. Exercicios/011 Matrizes/Atividades Extras/005/Exercicio_005/Exercicio_005/obj/Debug/net6.0/Exercicio_005.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio_005")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1865cd3949376262aec1c8a57716d196cad26a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio_005")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio_005")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
